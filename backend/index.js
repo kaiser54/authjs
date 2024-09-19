eb6105a3461f2d2f5da50c3f44f1b47b133590b8
Agboola Temitope <85450650+kaiser54@users.noreply.github.com>
2024-09-19 18:33:39 +0100

    chore: (BE) setting up server @backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
 import express from 'express'
+
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Hello world 123")
+} )
+
+app.listen(8000, () => {
+  console.log("server is running at port 8000")
+});
